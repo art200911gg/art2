062afeb837a687f70672906133d7d44da906bcab
art200911gg <157809093+art200911gg@users.noreply.github.com>
2024-01-26 15:45:22 +0200

    Create stile.css

---
+++ stile.css
@@ -0,0 +1,20 @@
+body{
+	background-color: brown;}
+
+a{
+	border: solid;
+}
+
+p{
+	background-color: red;
+}
+
+img {
+  border: 5px solid darkblue;
+}
+.box {
+  border: 5px solid darkblue;
+}
+img{
+
+}
